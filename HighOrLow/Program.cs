ccce2e023c27c313caf7772a506095483f1a7da0
Bilal Mirza <bbmirza117@gmail.com>
2024-07-09 16:17:40 -0400

    adding HighOrLow and MyFirstConsole apps

---
+++ HighOrLow/Program.cs
@@ -0,0 +1,56 @@
+class Program
+{
+	static void Main ( string[] args )
+	{
+		Console.WriteLine( "High/ Low Running" );
+	
+	// Computer Should choose random number
+	// User should be able to submit a guess
+	// if guess is too high then computer informs user
+	// if guess is too low then computer informs user
+	// a user wins when they guess the target number
+	// a user should keep guessing until they guess the correct target number
+	
+	//Variable
+	int targetNumber;
+	int guessNumber; 
+	int roundCount = 0;
+	string guessString;
+
+	// Creating a random number
+	Random rand = new Random();
+	targetNumber = rand.Next(11);
+
+	
+	do {
+		//roundCount = roundCount + 1;
+		//roundCount += 1;
+		roundCount++;
+
+		Console.Write( "Please enter a guess between -1 and 11: " );
+		guessString = Console.ReadLine();
+	
+		guessNumber = Int32.Parse( guessString );
+	
+		if( guessNumber == targetNumber )
+		{
+			Console.WriteLine( "You guessed correct, Congratulations!");
+		}
+	
+		else if( guessNumber > targetNumber )
+		{
+			Console.WriteLine( "Oops, too high!");
+		}
+	
+		else
+		{
+			Console.WriteLine( "Oops, too low!" );
+		}
+	
+	} while ( guessNumber != targetNumber );
+	Console.WriteLine( "Thanks for playing!" );
+	Console.WriteLine( " You took {0} rounds to guess the answer!", roundCount );
+
+	
+	}
+}
---
+++ myFirstConsole/Program.cs
@@ -0,0 +1,116 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+
+	public static void Main(string[] args) //ENTRYPOINT - Main Method- start point --
+	{
+	
+		/*
+		Method - a block of code/ group of code with a labeled functionality
+		Methods make a code readable for developers, and reusable - instead of writing the same line 
+		over and over, just "call" the method.
+		*/
+
+		Console.WriteLine("Hello, World!");
+
+		/*Object oriented programming - 
+			a paradigm of programming that bundles of data and values and behvaior to create an 
+			object. Application funcitonality is dependent on relationships between objects.
+
+		Variables - Data Types
+			C# is Strongly types - we need to declare what type of data is going to be 
+			in each variable
+		Compiling - 
+			taking the code we've written (in C#) and parsing that code into something the 
+			computer can underdtand.
+		Interpreted - 
+			"reads" the code line-by-line as it is running, and executes the application "on 
+			the fly". 
+		
+			(A compiled application will be faster than an interpreted application).
+		*/
+
+		Console.WriteLine("Please enter your name for a personalized greeting: ");
+		
+		// string type holds Text
+		// declaring a variable 
+		// <variable type> <name>
+
+		// instantiate a variable
+		// <variable> = <value>
+
+		string userName = "Bilal"; //declaration and instantiation
+		// userName = "Richard"; //assignment (re-assigning)
+		
+		userName = Console.ReadLine(); // Console is the object, Readline is the behavior.
+
+		// Console.WriteLine("Welcome to Revature: ");
+		// Console.WriteLine(userName);
+
+		// Console.WriteLine("Welcome to Revature: " + userName); //concatenation
+
+		Console.WriteLine("Welcome to Revature: {0}", userName); //Interpolation
+		Console.WriteLine( $"Welcome to Revature: {userName}"); //String Formating
+
+		/*
+			--Summary of Data Types--
+			
+			1 0 - binary
+			decimal - 5 ==> binary - 0101 (8,4,2,1 or 2^3,2^2,2^1,2^0)
+			
+			Numeric Data Types
+			double, float, long, short, decimal
+			Intigers - Whole Numbers
+
+			Boolean (true or false) - 1 or 0
+
+			Char -> Character (single)
+			String -> string of Text (words)
+
+			byte and bit (not as common, but they're still there)
+		*/
+
+		/*
+			--Conditonal statement and control flow--
+				if, else if, else
+				switch, case
+				try, catch, finally (exception handling)
+			
+			--looping--
+				for (specific number of loops)
+				do-while (executes once before checking condition)
+				while (checkss the condition before executing)
+				foreach (iterate through a collection)
+
+		*/
+		
+		bool? runChoice = null;
+		
+		if ( runChoice == true )
+		{
+			Console.WriteLine( "runChoice is true!" );
+		}
+
+		else if ( runChoice == false ) 
+		{
+			Console.WriteLine( "runChoice is false" );
+		}
+
+		else
+		{
+			Concole.WriteLine( "runChoice is undefined/null" );
+		}
+		
+		//Comparison operators ==, >, < , >=, <=, != (the ! - not - operator is very versatile!)
+
+		
+		if (5 > 4)
+		{
+			Console.WriteLine( "Five is greater than four" );
+		}
+		
+		
+			
+	}
+}
