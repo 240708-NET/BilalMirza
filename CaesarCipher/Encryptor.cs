21eb30d474de91d1365138aa215ce8e220fa4fc5
Bilal Mirza <bbmirza117@gmail.com>
2024-07-10 18:31:27 -0400

    Adding progress made to P0

---
+++ CaesarCipher/Encryptor.cs
@@ -0,0 +1,37 @@
+/*
+class Encryptor()
+{
+
+//
+
+
+//Fields
+//Variables
+	char [] alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',  'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+ 	
+
+	Console.WriteLine("Type '1' to encrypt, type '2' to decrypt: ")
+	string direction = Int32.Parse(Console.ReadLine());
+
+	Console.WriteLine("Type your message: ");
+	string message = console.ReadLine();
+	message = message.ToLower()
+
+	Console.WriteLine("Type the shift number: ");
+	string shift = Console.ReadLine();
+
+	void ceaser(direction, text, shift)
+	{
+	
+
+
+
+
+	}
+
+
+
+
+}
+
+*/
---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+	static void Main (string[] args )
+	{
+        //References variable containing Logo Design from the 'CaesarArt' class
+		Console.WriteLine ($"{CaesarArt.logo}");
+
+
+
+        Console.WriteLine( "GoodBye" );
+		Console.WriteLine( "You took {0} rounds to guess the answer!", roundCount );
+	}
+}
