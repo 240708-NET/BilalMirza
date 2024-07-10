6b5f32cf54fe2d187a4717ad6a51da936cf9cec8
Bilal Mirza <bbmirza117@gmail.com>
2024-07-10 14:13:02 -0400

    Adding personal project and oop practice files

---
+++ oopPractice/inheritence/Duck.cs
@@ -0,0 +1,16 @@
+public class Duck{
+
+	public string wings {get; set;}
+
+	public int weight {get; set;}	
+
+	public void Swim()
+	{
+		Console.WriteLine("Duck is Swimming");
+	}
+
+	public void Quack()
+	{
+		Console.WriteLine("Duck is Quacking");
+	}
+}
---
+++ oopPractice/inheritence/WoodDuck.cs
@@ -0,0 +1,5 @@
+namespace Inheritence;
+
+public class WoodDuck : Duck {
+
+}
