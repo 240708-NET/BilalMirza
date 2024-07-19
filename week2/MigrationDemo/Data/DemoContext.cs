664878a39400b74188440f50703f41daf90e6311
Bilal Mirza <bbmirza117@gmail.com>
2024-07-18 19:34:08 -0500

    Updating Migration Demo

---
+++ week2/MigrationDemo/Data/DemoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MigrationDemo.Models;
+
+public class DemoContext : DbContext{
+
+
+    // Allows exteran code to be passed in the configurations 
+    public DemoContext(DbContextOptions<DemoContext> options) : base(options){}
+
+    public DbSet<Departments> departments { get; set; }
+    public DbSet<Employees> employees { get; set; }
+
+
+}
