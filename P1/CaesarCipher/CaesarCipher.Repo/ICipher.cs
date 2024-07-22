6d44e33c27fefa9b95372a1fc08b499ca7e24c7a
Bilal Mirza <bbmirza117@gmail.com>
2024-07-21 23:48:05 -0500

    Adding an interface to P1

---
+++ P1/CaesarCipher/CaesarCipher.Repo/ICipher.cs
@@ -0,0 +1,13 @@
+﻿using CaesarCipher.Models;
+
+namespace CaesarCipher.Repo
+
+public interface ICipher
+{
+    int Id { get; set; }
+    string Message { get; set; }
+    int Shift { get; set; }
+    DateTime DateCreated { get; set; }
+    string Encrypt();
+    string Decrypt();
+}
