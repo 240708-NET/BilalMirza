6d8a648d933d4291adf144f34276ee88215418a7
Bilal Mirza <bbmirza117@gmail.com>
2024-07-22 00:45:08 -0500

    Fixed functionality for P0

--- P0/CaesarCipher/CaesarCipher.cs
+++ P0/CaesarCipher/CaesarCipher.cs
@@ -3,17 +3,23 @@ class CaesarCipher()
 	//Fields
 	char[] alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz".ToCharArray(); //double alphabet to handle forwards wrapping through alphatebt (test: 'encode' "xyz")
 
-	// Handles encryption of user input.
+	// encryptor: Handles encryption logic
+	// Direction - dictates wether user would like to encode or decode a message
+	// Message - Message user would like to encode
+	// Shift - the amount a user would like to shift each character in a message.
+
  	public string encryptor(string cipher_direction, string start_text, string shift_amount)
  	{
 		string end_text = "";
 		int position;
 		int new_position;
+		string direction = "encode";
 		int shiftNum = Int32.Parse(shift_amount) % 26; //Mod 26 to keep value in the bounds of the 'alphabet' array
 
 		if (cipher_direction == "decode")
 		{
 			shiftNum *= -1;
+			direction = "decode";
 		}
 
 		foreach (char c in start_text.ToLower()) //To.Lower to handle uppercase input
@@ -36,36 +42,10 @@ class CaesarCipher()
 				end_text += c; // Preserve spaces and other non-letter characters
 			}
 		}
-		return end_text;
+		return $"\n -- Here's the {direction}d result: {end_text} --";
 	}
 
-	// Checks user input to ensure that: Something is entered; intended input type is entered.
-	// Requirements: Please specify wether you are checking for "num" (numbers) or "char" characters
-	public string inputValidator (string type, string prompt)
-	{
-		Console.WriteLine (prompt);
-		string input = Console.ReadLine();
-
-		if (type == "char")
-			while (!input.All(char.IsLetter) || String.IsNullOrEmpty(input))
-			{
-				Console.WriteLine("\nInvalid input, please try again. [only characters are accepted]");
-				Console.WriteLine (prompt);
-				input = Console.ReadLine ();
-			}
-		else if (type == "num")
-		{
-			while (!input.All(char.IsDigit) || String.IsNullOrEmpty(input))
-			{
-				Console.WriteLine("\nInvalid input, please try again. [only numbers are accepted]");
-				Console.WriteLine (prompt);
-				input = Console.ReadLine ();
-			}
-		}
-		return input;
-	}
-
-	//Prompts user to decide wether to restart program or quit; Returns a boolean to signify continuation/ending of program.
+	//Prompts user to decide wether to restart program or quit; Returns a boolean to signify continuation/ ending of program.
 	public bool restartProgram ()
 	{
 		string choice = "";
