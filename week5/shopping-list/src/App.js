90c7812f4ecebbac73f9ce135ac3465e217115ad
Bilal Mirza <bbmirza117@gmail.com>
2024-08-06 14:25:22 -0500

    adding shopping list app in week5

---
+++ week5/shopping-list/src/App.js
@@ -0,0 +1,48 @@
+  import React, { useState } from 'react';
+  import './App.css';
+  const App = () => {
+    const [items, setItems] = useState(['ground beef', 'rice', 'bread']);
+
+    return (
+      <div id="list-container">
+        <ListDisplay items={items}/>
+        <InputText handleSubmit={(item) => {
+            setItems(items.concat(item));
+          }}
+        />
+      </div>
+    )
+  }
+
+  const ListItem = (props) => (
+    <li>{props.name}</li>
+  )
+
+  const ListDisplay = (props) => {
+    const items = props.items.map((item, i) => (
+      <ListItem
+        key={i}
+        name={item}
+      />
+    ))
+    return (
+      <ul>
+        {items}
+      </ul>
+    )
+  }
+
+  const InputText = (props) => {
+    const [value, setValue] = useState('');
+    return (
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        props.handleSubmit(value);
+        setValue('');
+      }}>
+      <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
+      </form>
+    )
+  }
+  
+  export default App;
